f396d17825d26b560b15cebddead85fbfed752f4
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-02 22:05:30 +0530

    add loggedin check in signin

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,6 +1,16 @@
 import { Form} from "../components/Form";
 import { Bio } from "../components/Bio";
+import { useContext, useEffect } from "react";
+import { Auth } from "../context/context";
+import { useNavigate } from "react-router-dom";
 export const Signin=()=>{
+  const navigate=useNavigate();
+  const {loggedin} =useContext(Auth);
+  useEffect(() => {
+    if(loggedin===null) return ;
+    if(loggedin===true) navigate("/");
+  }, [loggedin])
+  if(loggedin===null) return ;
     return <div className=" h-screen w-screen grid grid-cols-1 lg:grid-cols-2 "> 
           <div><Form type="signin"/></div>
           <div className="hidden lg:block"> <Bio/></div>
